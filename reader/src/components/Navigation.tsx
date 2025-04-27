f4a3c206ced1fe320cdb5611ca99eb411aaa9e5c
Daniel <daniel_sanchez18@my.vcccd.edu>
2025-04-27 02:58:42 -0700

    styling foro homepage and learn more page

--- reader/src/components/Navigation.tsx
+++ reader/src/components/Navigation.tsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const Navigation = () => {
   return (
     <nav className="fixed top-0 left-0 right-0 z-50 bg-background/80 backdrop-blur-lg border-b border-border">
-      <div className="max-w-7xl mx-auto px-6 h-16 flex items-center justify-between">
+      <div className="px-6 h-16 flex items-center justify-between">
         <Link to="/" className="text-xl font-semibold">
           readMe
         </Link>
